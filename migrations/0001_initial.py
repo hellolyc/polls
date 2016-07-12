2c67c819cbd9344d84701d7d4d6627647894f17e
hellolyc <995938715@qq.com>
2016-07-12 11:00:38 +0800

    addpolls

---
+++ migrations/0001_initial.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Choice',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('choice_text', models.CharField(max_length=200)),
+                ('votes', models.IntegerField(default=0)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Question',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('question_text', models.CharField(max_length=200)),
+                ('pub_date', models.DateTimeField(verbose_name=b'date published')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='User',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('username', models.CharField(max_length=20)),
+                ('password', models.CharField(max_length=20)),
+                ('date', models.DateTimeField(verbose_name=b'date created')),
+            ],
+        ),
+        migrations.AddField(
+            model_name='choice',
+            name='question',
+            field=models.ForeignKey(to='polls.Question'),
+        ),
+    ]
---
+++ views.py
@@ -0,0 +1,59 @@
+﻿from django.shortcuts import render
+from django.template import RequestContext,loader
+from models import Question, Choice
+from django.http import Http404
+from django.shortcuts import render,get_object_or_404
+from django.core.urlresolvers import reverse
+from django.views import generic
+from django.utils import timezone
+from mysite.views import check_login 
+# Create your views here.
+
+from django.http import HttpResponse,HttpResponseRedirect
+@check_login
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = RequestContext(request,{'latest_question_list': latest_question_list})
+    return HttpResponse(template.render(context))
+@check_login
+def detail(request,question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+            raise Http404("Question does not exist")
+    return render(request,'polls/detail.html',{'question' : question})
+@check_login
+def results(request, question_id):
+    question = get_object_or_404(Question,pk = question_id)
+    return render(request,'polls/result.html',{'question' : question})
+@check_login
+def vote(request,question_id):
+    p = get_object_or_404(Question,pk = question_id)
+    try:
+        selected_choice = p.choice_set.get(pk = request.POST['choice'])
+    except (KeyError,Choice.DoesNotExist):
+        return render(request,'polls/detail.html',{
+            'question' : p,
+            'error_message' : "You didn't select a choice.", 
+            })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+    return HttpResponseRedirect(reverse('polls:results',args=(p.id,)))
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+    def get_queryset(self):
+        return Question.objects.filter(
+            pub_date__lte = timezone.now()
+            ).order_by("-pub_date")[:5]
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte = timezone.now())
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/result.html'
+    
